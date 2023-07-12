9bfab19ace2e464bc41d42c78e13b7d5afe8c555
hd2yao <1054290355@qq.com>
2023-07-12 17:56:34 +0800

    feat:cache

--- cache.go
+++ cache.go
@@ -1,5 +1,10 @@
 package cache
 
+import (
+    "log"
+    "sync"
+)
+
 // Cache 缓存接口
 type Cache interface {
     Set(key string, value interface{}) // 设置/添加一个缓存，如果 key 存在，用新值覆盖旧值
@@ -7,4 +12,57 @@ type Cache interface {
     Del(key string)                    // 通过 key 删除一个缓存值
     DelOldest()                        // 删除最"无用"的一个缓存值
     Len() int                          // 获取缓存已存在的记录数
+}
+
+// DefaultMaxBytes 默认允许占用的最大内存
+const DefaultMaxBytes = 1 << 29
+
+// safeCache 并发安全缓存
+type safeCache struct {
+    m     sync.RWMutex
+    cache Cache
+
+    nhit, nget int
+}
+
+func newSafeCache(cache Cache) *safeCache {
+    return &safeCache{
+        cache: cache,
+    }
+}
+
+func (sc *safeCache) Set(key string, value interface{}) {
+    sc.m.Lock()
+    defer sc.m.Unlock()
+    sc.cache.Set(key, value)
+}
+
+func (sc *safeCache) Get(key string) interface{} {
+    sc.m.Lock()
+    defer sc.m.Unlock()
+    sc.nget++
+    if sc.cache == nil {
+        return nil
+    }
+
+    v := sc.cache.Get(key)
+    if v != nil {
+        log.Println("[TourCache] hit")
+        sc.nhit++
+    }
+
+    return v
+}
+
+func (sc *safeCache) stat() *Stat {
+    sc.m.RLock()
+    defer sc.m.RUnlock()
+    return &Stat{
+        NHit: sc.nhit,
+        NGet: sc.nget,
+    }
+}
+
+type Stat struct {
+    NHit, NGet int
 }
